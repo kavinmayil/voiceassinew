723f6d471e9c723367b5cf1170f1e20ca360ca41
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-05-08 16:09:42 +0530

    Create jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,37 @@
+speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            music_dir = 'G:\\kishan all.co\\d\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\kishancjx\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+            #you should enter your path here
+
+        elif 'email to ' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "YourfriendEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend. I am not able to send this email")
